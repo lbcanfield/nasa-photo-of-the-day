9230dbb179abb75e8f98b3ea90ba1e0b43920f62
lbcanfield <lewie.b.canfield@gmail.com>
2022-10-31 09:35:53 -0500

    Installed axios dependancy; imported useState and useEffect from react; created a slice of state variable for the api data that will be accessed later; created a dummyData folder with a sample data from the api request so that the maximum number of requests are not reached.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
+import dummyData from './dummyData/dummyData';
 
 function App() {
+  // Variables local to the App component.
+  const [someVar, setSomeVar] = useState(dummyData);
+  console.log( someVar );
+  
+  /*useEffect(() => 
+  {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then( response => 
+      {
+        console.log( response );
+      })
+    .catch( error  => console.error( error ))
+
+  }, [])  //Using empty dependancy array to run once   */
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        {/* Read through the instructions in the README.md file to build your NASA */}
+        {/* app! Have fun <span role="img" aria-label='go!'>🚀</span>! */}
       </p>
     </div>
   );
 }
 
 export default App;
+
+
