e1678828e389c160281c19afd6d2a901e86cb57b
lbcanfield <lewie.b.canfield@gmail.com>
2022-10-31 18:21:50 -0500

    used axios to get api data from the nasa picture of the day api.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
-import testData from './dummyData/dummyData';
+// import testData from './dummyData/dummyData';
 import "./App.css";
 import BuildPage from "./components/nasaPOTD";
 
 function App() {
-  const [data, setData] = useState(testData);
+  const [data, setData] = useState();
+
+  useEffect( () => 
+  {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then( response => 
+        {
+          setData(response.data);
+        })
+      .catch( error => console.error( error ))
+  }, [] )    
   return (
     <div className="App">
       { data && <BuildPage data={data} /> }  {/*short circuit evaluation */}
