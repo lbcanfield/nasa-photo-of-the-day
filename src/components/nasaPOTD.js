095405268b2b27cdb1be1292e2ff1f31a805f193
lbcanfield <lewie.b.canfield@gmail.com>
2022-11-01 09:32:05 -0500

    Installed additional dependancies including styled-components.  Updated the styles to use the styled-components react module.

--- src/components/nasaPOTD.js
+++ src/components/nasaPOTD.js
@@ -1,20 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
+const StyledContent = styled.section
+`   display: flex;
+    align-items: center;
+`;
+
+const StyledDiv = styled.div 
+`   padding: 25px;
+`;
+
+const StyledImg = styled.img 
+`   max-width: 100%;
+`;
+//.contentWrapper img
+//{
+//  max-width: 100%;
+//}
 
 const BuildPage = (props) =>
 {
     // console.log( props );
     
     return (
-        <section className='contentWrapper'>
-            <div className='contentText'>
+        <StyledContent className='contentWrapper'>
+            <StyledDiv className='contentText'>
                 <h2>{props.data.title}</h2>
                 <p>{props.data.date}</p>
                 <p>{props.data.explanation}</p>
-            </div>
-            <div className='contentImg'>
-                <img src={props.data.hdurl}></img>
-            </div>
-        </section>
+            </StyledDiv>
+            <StyledDiv className='contentImg'>
+                <StyledImg src={props.data.hdurl}></StyledImg>
+            </StyledDiv>
+        </StyledContent>
     );
 }
 
